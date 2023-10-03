3472bc923f821ecc712884f74e9101aee297a7dc
KmIndu <73270523+KmIndu@users.noreply.github.com>
2023-10-03 07:53:31 -0700

    Add files via upload

---
+++ day1/Variable.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class Variable {
+	public static void main(String[] args) {
+		int intVal=0;
+		float floatVal=0.0f;
+		double doubleVal = 0.0;
+		char charVal = 'a';
+		boolean boolVal = true;
+		System.out.println(intVal);
+		System.out.println(floatVal);
+		System.out.println(doubleVal);
+		System.out.println(charVal);
+		System.out.println(boolVal);
+	}
+}
---
+++ day1/Welcome.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("welcome to Object Oriented Programming: Day 1 Session");
+	}
+}
